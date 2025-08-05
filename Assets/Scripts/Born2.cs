0bac128e5193f0460f3c9840af3c6e8570131e3e
xj <goliathrearmed@gmail.com>
2025-08-05 16:57:14 +0800

    Init commit

---
+++ Assets/Scripts/Born2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Born2 : MonoBehaviour
+{
+    public GameObject playerPrefab2;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("BornTank", 1);
+        Destroy(gameObject, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BornTank()
+    {
+
+        Instantiate(playerPrefab2, transform.position, Quaternion.identity);
+
+    }
+}
